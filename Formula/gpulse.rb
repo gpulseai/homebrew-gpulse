7b110e61926889071e3f0d32564130f6f5fc8ab7
Karthik Kirubakaran <karthik.kirubakaran@fastqlik.com>
2026-03-23 11:07:40 -0400

    Update gpulse to v2.2.0

--- Formula/gpulse.rb
+++ Formula/gpulse.rb
@@ -1,13 +1,13 @@
 class Gpulse < Formula
   desc "Real-time GPU monitoring with leak detection and OOM prediction"
   homepage "https://gpulse.ai"
-  version "2.1.0"
+  version "2.2.0"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gpulseai/gpulse/releases/download/v2.1.0/gpulse-v2.1.0-darwin-arm64.tar.gz"
-      sha256 "9c7b9276af39c46d9190514a6038723f71d743aa8fe8c1d81d57c9760b1260d2"
+      url "https://github.com/gpulseai/gpulse/releases/download/v2.2.0/gpulse-v2.2.0-darwin-arm64.tar.gz"
+      sha256 "c0264a1c5d8cc3373d490b4fdabbe96397cfbb3cae8cd85d675c653e1d06251e"
     end
   end
 
